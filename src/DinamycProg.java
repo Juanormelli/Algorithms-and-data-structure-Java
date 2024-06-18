aa62416758b83a3cbd67feec93aaf2f0e28b2970
Juan Medeiros <juan_ormelli@hotmail.com>
2024-06-18 11:06:26 -0300

    Algoritimo de programação dinamica

---
+++ src/DinamycProg.java
@@ -0,0 +1,111 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DinamycProg {
+
+    public static void main(String[] args) {
+        var list = new HashMap<>();
+        list.put("Water", new int[2]);
+        list.put("Book", new int[2]);
+        list.put("Food", new int[2]);
+        list.put("Casaco", new int[2]);
+        list.put("Camera", new int[2]);
+
+        var enumerator = new HashMap<>();
+        enumerator.put(0, "Water");
+        enumerator.put(1, "Book");
+        enumerator.put(2, "Food");
+        enumerator.put(3, "Casaco");
+        enumerator.put(4, "Camera");
+
+        int[] Water = (int []) list.get("Water");
+        Water[0] = 3;
+        Water[1] = 10;
+
+        int[] Book = (int []) list.get("Book");
+        Book[0] = 1;
+        Book[1] = 3;
+
+        int[] Food = (int []) list.get("Food");
+        Food[0] = 2;
+        Food[1] = 9;
+
+        int[] Casaco = (int []) list.get("Casaco");
+        Casaco[0] = 2;
+        Casaco[1] = 5;
+
+        int[] Camera = (int []) list.get("Camera");
+        Camera[0] = 1;
+        Camera[1] = 6;
+
+        System.out.println(BestDecision(list, 6, enumerator));
+
+        List best = BestDecision(list, 6, enumerator);
+
+        //System.out.println(best.get(1));
+    }
+
+    public static List<String> BestDecision(HashMap integerList, int maxKg, HashMap lhmEnumarator){
+        int Best = 0;
+        int[][] tab = new int[integerList.size()][6];
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i <=integerList.size() ; i++) {
+            for (int j = 1; j <= maxKg ; j++) {
+                int[] teste = (int[]) integerList.get(lhmEnumarator.get(i));
+                if (teste == null){
+                    return names;
+                }
+                if(i == 0){
+                    if(j == maxKg){
+                         names.add((String)lhmEnumarator.get(i));
+                    }
+
+                    if (teste[0] <=  j){
+                        tab[i][j - 1] = teste[1];
+                        Best = teste[1];
+
+
+                    }
+                    else {
+                        tab[i][j - 1] = 0;
+                    }
+
+
+                }
+                else {
+                    if (teste[0] <=  j) {
+                        if(j - 1 - teste[0] < 0){
+                            if(tab[i - 1][j - 1] > teste[1]){
+                                tab[i][j - 1] = tab[i - 1][j - 1];
+                            }
+                            else{
+                                tab[i][j - 1] = teste[1];
+                            }
+
+                        }
+                        else {
+                            tab[i][j - 1] = Math.max(tab[i - 1][j - 1], teste[1] + tab[i - 1][j - 1 - teste[0]]);
+
+                            if (tab[i][j - 1] > Best) {
+                                Best = tab[i][j - 1];
+                                names.remove(0);
+                                names.add((String) lhmEnumarator.get(i));
+
+                            }
+                        }
+                    }
+                    else {
+                        tab[i][j - 1] = tab[i - 1][j - 1];
+                    }
+
+                }
+
+
+            }
+
+        }
+        System.out.println(names);
+        return names;
+    }
+}
